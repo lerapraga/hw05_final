705c8303a90fdacde49fa78ef70d1285270ad8b2
Lera Praga <abastrenie@mail.ru>
2023-02-01 21:20:08 +0500

    fix sequrity problems and ready to 2 round

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECRET_KEY in gitignore
+SECRET_KEY = 'secret_key.py'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False  # turned off
